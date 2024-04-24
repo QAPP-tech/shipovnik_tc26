a9139ef6178a6dfebac3ae328817a361f0e85256
Maxim Kot <mkot@qapp.tech>
2024-04-24 20:24:00 +0300

    Fix error by one in example

--- shipovnik_example.c
+++ shipovnik_example.c
@@ -52,12 +52,13 @@ int main(int argc, char *argv[]) {
 
   uint8_t sig[SHIPOVNIK_SIGBYTES];
   size_t sig_len;
-  shipovnik_sign(sk, msg, sizeof(msg), sig, &sig_len);
+  shipovnik_sign(sk, msg, sizeof(msg) - 1, sig, &sig_len);
 
   puts("SIGNATURE");
   dump_bstr(sig, sig_len);
 
-  int ret = shipovnik_verify(pk, sig, msg, sizeof(msg));
+  int ret = shipovnik_verify(pk, sig, msg, sizeof(msg) - 1);
+
   if (ret == 0) {
     puts("VERIFY: SUCCESS");
   } else {
